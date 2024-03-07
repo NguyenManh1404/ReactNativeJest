4c622540ba1d8d730299736eafcb86144a2f1151
manh.nguyen <manh.nguyen@manhnguyen.local>
2024-03-07 14:42:02 +0700

    login with microsoft msauth.com.reactnativejest://auth/

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,38 @@
-import {StyleSheet, SafeAreaView, Button} from 'react-native';
+import {StyleSheet, SafeAreaView, Button, Platform} from 'react-native';
 import React from 'react';
 import {SCREENS} from '../../App';
 import {useNavigation} from '@react-navigation/native';
+import {authorize} from 'react-native-app-auth';
+//hungmanh14042001@gmail.com
+const config = {
+  serviceConfiguration: {
+    authorizationEndpoint:
+      'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+    tokenEndpoint: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+  },
+  clientId: 'cfce8715-42fc-473b-9106-b4dcac16014b',
+  redirectUrl: Platform.select({
+    android: 'msauth://com.reactnativejest/ZyQno6t2zOJ8MS2zyGfIqWrqAxs%3D',
+    ios: 'msauth.com.reactnativejest://auth/',
+  }),
+  scopes: ['openid', 'profile', 'email', 'offline_access'],
+};
 
 const HomeScreen = () => {
   const {navigate} = useNavigation();
+
+  const loginMicrosoft = async () => {
+    try {
+      // console.log("vao day")
+      const authState = await authorize(config);
+      console.log('🚀 ~ loginMicrosoft ~ authState:', authState);
+    } catch (error) {
+      // console.log('vao day eee');
+      console.log('error', error);
+    }
+  };
+  // Log in to get an authentication token
+
   return (
     <SafeAreaView style={styles.container}>
       {Object.keys(SCREENS).map((key, index) => {
@@ -22,8 +50,8 @@ const HomeScreen = () => {
         );
       })}
 
-      {/* <Button title="Login Screen" color={'red'} />
-      <Button title="Counter" color={'green'} />
+      <Button title="Login Microsft" color={'red'} onPress={loginMicrosoft} />
+      {/* <Button title="Counter" color={'green'} />
       <Button title="Video" color={'blue'} /> */}
     </SafeAreaView>
   );
